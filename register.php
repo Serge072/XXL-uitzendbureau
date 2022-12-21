d413e9ac5303112d872bb23a3c13e5b1a5e73bb7
Serge072 <78144801+Serge072@users.noreply.github.com>
2022-12-21 10:57:37 +0100

    login pagina

--- register.php
+++ register.php
@@ -14,29 +14,52 @@
 
 
 <form action="register.php" method="post">
-  <label for="username">Username:</label><br>
-  <input type="text" id="username" name="username"><br>
-  <label for="password">Password:</label><br>
-  <input type="password" id="password" name="password"><br><br>
+  <label for="first_name">Voornaam:</label><br>
+  <input type="text" id="first_name" name="first_name" required><br>
+  <label for="last_name">Achternaam:</label><br>
+  <input type="text" id="last_name" name="last_name" required><br>
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email" required><br>
+  <label for="phone">Telefoonnummer:</label><br>
+  <input type="text" id="phone" name="phone" required><br>
+  <label for="address">Adres:</label><br>
+  <input type="text" id="address" name="address" required><br>
+  <label for="company_name">Bedrijfsnaam:</label><br>
+  <input type="text" id="company_name" name="company_name"><br>
+  <label for="password">Wachtwoord:</label><br>
+  <input type="password" id="password" name="password" required><br>
+  <label for="type">Hoe wilt u registreren?</label>
+  <select name="type" id="type">
+    <option value="company">Bedrijf</option>
+    <option value="customer">Klant</option>
+  </select>
+  <br><br>
   <input type="submit" value="Submit">
 </form> 
+
 
 
 <?php
   // Check if the form was submitted
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // Get the submitted form data
-    $username = $conn->real_escape_string($_POST['username']);
-    $password = $conn->real_escape_string($_POST['password']);
-
-    // Insert the new user into the database
-    $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-    $result = $conn->query($query);
+      // Get the submitted form data
+      $password = $conn->real_escape_string($_POST['password']);
+      $type = $conn->real_escape_string($_POST['type']);
+      $email = $conn->real_escape_string($_POST['email']);
+      $phone = $conn->real_escape_string($_POST['phone']);
+      $first_name = $conn->real_escape_string($_POST['first_name']);
+      $last_name = $conn->real_escape_string($_POST['last_name']);
+      $address = $conn->real_escape_string($_POST['address']);
+      $company_name = $conn->real_escape_string($_POST['company_name']);
+  
+  // Insert the new user into the database
+  $query = "INSERT INTO users (password, type, email, phone, first_name, last_name, address, company_name) VALUES ('$password', '$type', '$email', '$phone', '$first_name', '$last_name', '$address', '$company_name')";
+  $result = $conn->query($query);
 
     if ($result) {
       // The registration was successful
-      echo '<p>Registration successful!</p>';
+      echo '<p>U bent succesvol geregistreed!</p>';
     } else {
       // The registration was unsuccessful
       echo '<p>Error: ' . $conn->error . '</p>';
